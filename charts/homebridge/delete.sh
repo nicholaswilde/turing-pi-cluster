c3958feea54a71208a129d768a583085afbe79c7
Nicholas Wilde <ncwilde43@gmail.com>
2020-10-26 13:14:16 -0700

    Update homebridge deploy and delete scripts

--- charts/homebridge/delete.sh
+++ charts/homebridge/delete.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if ! command -v kubectl &> /dev/null; then
+    echo 'kubectl is not installed'
+    exit 1
+fi
+
+if ! command -v helm &> /dev/null; then
+    echo 'helm is not installed'
+    exit 1
+fi
+
 helm delete homebridge
 kubectl delete pvc homebridge-pvc -n homebridge
 kubectl delete pv homebridge-pv-nfs -n homebridge
--- charts/homebridge/deploy.sh
+++ charts/homebridge/deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if ! command -v kubectl &> /dev/null; then
+    echo 'kubectl is not installed'
+    exit 1
+fi
+
+if ! command -v helm &> /dev/null; then
+    echo 'helm is not installed'
+    exit 1
+fi
+
 kubectl apply -f namespace.yaml
 kubectl apply -f pv.yaml
 kubectl apply -f pvc.yaml
