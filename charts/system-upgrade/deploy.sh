cb36b79ee8d100c9f83b389bf5e651f9785fe309
Nicholas Wilde <ncwilde43@gmail.com>
2020-10-30 22:41:47 -0700

    Added watch command to end of deploy

--- charts/system-upgrade/deploy.sh
+++ charts/system-upgrade/deploy.sh
@@ -2,3 +2,4 @@
 
 kubectl apply -f https://raw.githubusercontent.com/rancher/system-upgrade-controller/master/manifests/system-upgrade-controller.yaml
 kubectl apply -f ./plans.yaml
+watch kubectl get all -n system-upgrade
